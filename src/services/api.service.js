6e06083b742dc35203ccbb3fa76918e216cf431d
Tony Mai <tonay6@gmail.com>
2022-11-14 19:48:40 -0600

    add api service

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -14,5 +14,27 @@
 const BASE_URL = "http://api.github.com";
 
 export default {
+  getIssuesResponse(nameOfRepo) {
+    // Nothing to await, just return the promise.
+    // WHoever calls this will await it.
+    return fetch(`${BASE_URL}/repos/${nameOfRepo}/issues?direction=asc`);
+  },
+
+  async getReposByTopic(topic) {
+    const response = await fetch(
+      `${BASE_URL}/search/repositories?q=${topic}+is:featured&sort=help-wanted-issues`
+    );
+
+    // This Promise will need to be awaited by the caller
+    return response.json();
+  },
   // TODO: Add methods for getting repos by topic, issues, and user repos
 };
+
+
+async getReposByUsername(username) {
+  const response = await fetch(`${BASE_URL}/users/${username}/repos`);
+
+  // This Promise will need to be awaited by the caller.
+  return response.json();
+}
